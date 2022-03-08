bfd351143f5a318bcfafb4a9ddde2f2b9b355162
Himanshu Choudhary <raptordev21@gmail.com>
2022-03-08 12:26:21 +0530

    added backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,39 @@
-console.log('hello world')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const morgan = require('morgan')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+// Dev logging middleware 'morgan'
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
+// Serve frontend
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+//   app.get('*', (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+//     )
+//   )
+// } else {
+//   app.get('/', (req, res) => res.send('Please set to production'))
+// }
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
